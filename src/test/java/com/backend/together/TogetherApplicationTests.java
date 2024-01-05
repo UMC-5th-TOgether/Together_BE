c88f810b4a9e484b66e2e8e901f54da62fad748a
hyunjin9603 <hyunjin9603@gmail.com>
2024-01-05 16:42:16 +0900

    폴더생성

---
+++ src/test/java/com/backend/together/TogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
